6460094e8caf79ebe52e8b4dd352c98b6e0e3b46
ryota-kita <ryota-kita@kke.co.jp>
2021-11-06 11:24:15 +0900

    断面モデルにJを追加した。

--- src/element/section.rs
+++ src/element/section.rs
@@ -3,11 +3,12 @@ pub struct Section {
     Iy: f64,
     Iz: f64,
     K: f64,
+    J: f64,
 }
 
 impl Section {
-    pub fn new(A: f64, Iy: f64, Iz: f64, K: f64) -> Self {
-        Section { A, Iy, Iz, K }
+    pub fn new(A: f64, Iy: f64, Iz: f64, K: f64, J: f64) -> Self {
+        Section { A, Iy, Iz, K, J }
     }
 
     pub fn get_A(&self) -> f64 {
@@ -25,6 +26,10 @@ impl Section {
     pub fn get_K(&self) -> f64 {
         self.K
     }
+
+    pub fn get_J(&self) -> f64 {
+        self.J
+    }
 }
 
 #[cfg(test)]
@@ -32,10 +37,11 @@ mod tests {
     #[test]
     fn create_instance() {
         use super::*;
-        let section = Section::new(100.0, 1000.0, 10000.0, 0.0);
+        let section = Section::new(100.0, 1000.0, 10000.0, 0.0, 1.0);
         assert_eq!(section.A, 100.0);
         assert_eq!(section.Iy, 1000.0);
         assert_eq!(section.Iz, 10000.0);
         assert_eq!(section.K, 0.0);
+        assert_eq!(section.J, 1.0);
     }
 }
